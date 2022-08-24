290f0c552d132bebcc528cba3b7bfb0f0f2ff863
Raymond Julin <raymond.julin@gmail.com>
2022-08-24 12:27:37 +0200

    fix: Attempt to fix output of secrets that block setting outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,12 +38,12 @@ fi
 if ! flyctl status --app "$app"; then
   flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
   if [ -n "$INPUT_SECRETS" ]; then
-    echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
+    flyctl secrets set --app "$app" $INPUT_SECRETS
   fi
   flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
 elif [ "$INPUT_UPDATE" != "false" ]; then
   if [ -n "$INPUT_SECRETS" ]; then
-    echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
+    flyctl secrets set --app "$app" $INPUT_SECRETS
   fi
   flyctl deploy --app "$app" --region "$region" --image "$image" --region "$region" --strategy immediate
 fi
